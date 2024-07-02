3c3f1e84562736fc3db8285b31410455c3fdb259
jitae-moon <jitae.moon@icloud.com>
2024-07-02 21:48:26 +0900

    Chapter05 datatype

---
+++ section01/chapter05.js
@@ -0,0 +1,18 @@
+// Number type
+let num1 = 72;
+let num2 = 28;
+
+let inf = Infinity;
+let mInf = -Infinity;
+let nan = NaN;
+
+// String type
+let myName = 'moon';
+let character = 'perfect';
+let me = myName + character;
+
+let backtick = `my last name is ${myName}`; // Template literal
+
+// Boolean type
+let isGood = true;
+let isBad = false;
