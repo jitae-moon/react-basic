810225bd8eb6393ba494a7f8b74944ea1ca0319f
jitae-moon <jitae.moon@icloud.com>
2024-08-09 15:07:10 +0900

    JSX syntax

--- section05/src/components/Main.jsx
+++ section05/src/components/Main.jsx
@@ -1,9 +1,33 @@
+import './Main.css';
+// JSX 주의사항
+// 1. 중괄호 내부에는 js 표현식만 가능
+// 2. 숫자, 문자열, 배열값만 렌더링 됨
+// 3. 모든 태그는 닫혀야 함
+// 4. 최상위 태그는 반드시 한 개
 const Main = () => {
-    return (
-        <main>
-            <h1>main</h1>
-        </main>
-    );
+    const user = {
+        name: 'moon',
+        isLoggedIn: false
+    };
+
+    // return (
+    //     <>
+    //     {user.isLoggedIn ? (
+    //         <div>Logout</div>
+    //     ) : (
+    //         <div>Log in</div>
+    //     )}
+    //     </>
+    // );
+
+    if (user.isLoggedIn) {
+        return <div style={{
+            backgroundColor: 'red',
+            borderBottom: '5px solid blue'
+        }}>Logout</div>;
+    } else {
+        return <div className='login'>Log in</div>;
+    }
 }
 
 export default Main;
