7f37e183bd0b9339efe4637b0c95b7113abca4ee
jitae-moon <jitae.moon@icloud.com>
2024-08-11 00:13:56 +0900

    State & Props

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,27 +1,17 @@
 import './App.css';
 import {useState} from 'react';
+import Bulb from './components/Bulb';
+import Counter from './components/Counter';
 
 // Parent component & Root Component
 function App() {
-  const [count, setCount] = useState(0);
-  const [light, setLight] = useState('OFF');
 
   return (
     <>
     <div>
-      <h1>{light}</h1>
-      <button
-      onClick={() => {
-        setLight(light === 'ON' ? 'OFF' : 'ON');
-      }}
-      >{light === 'ON' ? 'OFF' : 'ON'}</button>
+    <Bulb />
     </div>
-    <div>
-    <h1>{count}</h1>
-    <button onClick={() => {
-      setCount(count + 1);
-    }}>+</button>
-    </div>
+    <Counter />
     </>
   );
 }
---
+++ section05/src/components/Bulb.jsx
@@ -0,0 +1,22 @@
+import {useState} from 'react';
+
+const Bulb = () => {
+  const [light, setLight] = useState('OFF');
+  return (
+  <>
+  <div>{light === 'ON' ? 
+  (
+  <h1 style={{backgroundColor: "orange"}}>ON</h1> 
+  ) : (
+  <h1 style={{backgroundColor: "gray"}}>OFF</h1>)}
+  </div>
+      <button
+      onClick={() => {
+        setLight(light === 'ON' ? 'OFF' : 'ON');
+      }}
+      >{light === 'ON' ? 'OFF' : 'ON'}</button>
+  </>
+  );
+};
+
+export default Bulb;
