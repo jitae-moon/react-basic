b7120646c91d6c006827e891c0a84e619583ffe2
jitae-moon <jitae.moon@icloud.com>
2024-08-13 10:35:48 +0900

    Add project basic components

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Viewer from './components/Viewer';
+import Controller from './components/Controller';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    Counter app
-    </>
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer />
+      </section>
+      <section>
+        <Controller />
+      </section>
+    </div>
   )
 }
 
